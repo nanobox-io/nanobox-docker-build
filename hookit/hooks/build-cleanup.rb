de7c25ae6926d7606f6f233d6728d2daf67c0eac
Greg Linton <lintong@pagodabox.com>
2015-06-17 15:34:08 -0600

    Cache the pkgin downloads, cleaning them out of the deploy

--- hookit/hooks/build-cleanup.rb
+++ hookit/hooks/build-cleanup.rb
@@ -1,7 +1,17 @@
 env_vars = File.read("/etc/environment.d/nanobox")
+
 execute "run cleanup" do
   command '/var/nanobox/engines/cleanup'
   cwd '/data'
+  environment env_vars
+  path GOPAGODA_PATH
+  user 'gopagoda'
+  stream true
+  on_data {|data| logvac.print data}
+end
+
+execute "Cache downloads" do
+  command 'mv /data/var/db/pkgin/. /var/nanobox/cache/'
   environment env_vars
   path GOPAGODA_PATH
   user 'gopagoda'
