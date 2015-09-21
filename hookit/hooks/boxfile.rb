2c71bcbc81a4e05a63ea65afbe7fec57dfa572af
Tyler Flint <tylerflint@gmail.com>
2015-09-21 15:56:33 -0600

    printing stderr to logtap for boxfile script

--- hookit/hooks/boxfile.rb
+++ hookit/hooks/boxfile.rb
@@ -7,17 +7,20 @@ logtap.print(bullet('Running boxfile hook'), 'debug')
 # By this point, engine should be set in the registry
 engine = registry('engine')
 
-boxfile = begin
-  if ::File.exist? "#{ENGINE_DIR}/#{engine}/bin/boxfile"
+boxfile = ''
 
-    logtap.print bullet('Boxfile script detected, running now'), 'debug'
+if ::File.exist? "#{ENGINE_DIR}/#{engine}/bin/boxfile"
 
-    execute "generating boxfile" do
-      command %Q(#{ENGINE_DIR}/#{engine}/bin/boxfile '#{engine_payload}')
-      cwd "#{ENGINE_DIR}/#{engine}/bin"
-      path GONANO_PATH
-      user 'gonano'
-    end
+  logtap.print bullet('Boxfile script detected, running now'), 'debug'
+
+  execute "generating boxfile" do
+    command %Q(#{ENGINE_DIR}/#{engine}/bin/boxfile '#{engine_payload}')
+    cwd "#{ENGINE_DIR}/#{engine}/bin"
+    path GONANO_PATH
+    user 'gonano'
+    stream true
+    on_stderr {|data| logtap.print data}
+    on_stdout {|data| boxfile << data}
   end
 end
 
