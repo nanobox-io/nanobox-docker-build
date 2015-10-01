85d74e6b1ba15926d3e5f9a9d3927d7af58914fb
Tyler Flint <tylerflint@gmail.com>
2015-10-01 11:52:47 -0600

    adding a user hooks to custom configure the user environment and keys

---
+++ hookit/hooks/user.rb
@@ -0,0 +1,30 @@
+# import some logic/helpers from lib/*.rb
+include NanoBox::Engine
+
+# ensure the gonano .ssh directory exists
+directory "/home/gonano/.ssh" do
+  parent true
+  mode 700
+  owner 'gonano'
+  group 'gonano'
+end
+
+# set custom ssh configuration
+hook_file "/home/gonano/.ssh/config" do
+  source 'ssh/config'
+  mode 600
+  owner 'gonano'
+  group 'gonano'
+end
+
+# create file for each file passed in
+payload[:ssh_files].each do |name, body|
+
+  file "/home/gonano/.ssh/#{name}" do
+    content body
+    mode 600
+    owner 'gonano'
+    group 'gonano'
+  end
+
+end
