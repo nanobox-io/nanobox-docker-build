6a972c3a2a90bd52cb1db8dba64f4fd3b7bb7a5c
Daniel Barney <daniel@pagodabox.com>
2015-10-15 10:17:58 -0600

    we don't need an interactive tty

--- tests/boxfile_empty_payload_test.sh
+++ tests/boxfile_empty_payload_test.sh
@@ -5,8 +5,8 @@ pass "unable to start the $VERSION container" docker run --privileged=true -d --
 
 defer docker kill $UUID
 
-pass "create hookit db folder" docker exec -it $UUID mkdir -p /var/db/hookit
+pass "create hookit db folder" docker exec $UUID mkdir -p /var/db/hookit
 
-pass "unable to set engine" docker exec -it $UUID bash -c 'echo '"'"'{"engine":"3-php"}'"'"' > /var/db/hookit/db.json'
+pass "unable to set engine" docker exec $UUID bash -c 'echo '"'"'{"engine":"3-php"}'"'"' > /var/db/hookit/db.json'
 
-pass "unable to run boxfile with empty payload" docker exec -it $UUID /opt/bin/default-boxfile '{}'
+pass "unable to run boxfile with empty payload" docker exec $UUID /opt/bin/default-boxfile '{}'
