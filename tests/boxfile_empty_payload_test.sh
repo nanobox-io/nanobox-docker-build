dd4e419833c108a576eb06d596c8ecf0f7a7349c
Braxton Huggins <braxton@pagodabox.com>
2015-10-15 09:59:47 -0600

    fix the test

--- tests/boxfile_empty_payload_test.sh
+++ tests/boxfile_empty_payload_test.sh
@@ -5,6 +5,8 @@ pass "unable to start the $VERSION container" docker run --privileged=true -d --
 
 defer docker kill $UUID
 
+pass "create hookit db folder" docker exec -it $UUID mkdir -p /var/db/hookit
+
 pass "unable to set engine" docker exec -it $UUID bash -c 'echo '"'"'{"engine":"3-php"}'"'"' > /var/db/hookit/db.json'
 
 pass "unable to run boxfile with empty payload" docker exec -it $UUID /opt/bin/default-boxfile '{}'
