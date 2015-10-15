02004759b94b0d351e903f53dbb7a0485e063390
Braxton Huggins <braxton@pagodabox.com>
2015-10-15 09:55:30 -0600

    fix the test, and fix logtap with no host issue

--- tests/boxfile_empty_payload_test.sh
+++ tests/boxfile_empty_payload_test.sh
@@ -3,6 +3,8 @@ UUID=$(cat /proc/sys/kernel/random/uuid)
 
 pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/build
 
-pass "unable to run boxfile with empty payload" docker exec -it $UUID /opt/bin/default-boxfile '{}'
-
 defer docker kill $UUID
+
+pass "unable to set engine" docker exec -it $UUID bash -c 'echo '"'"'{"engine":"3-php"}'"'"' > /var/db/hookit/db.json'
+
+pass "unable to run boxfile with empty payload" docker exec -it $UUID /opt/bin/default-boxfile '{}'
