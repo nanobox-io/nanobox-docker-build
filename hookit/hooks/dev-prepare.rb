490b6b0b40a62c4d5d55c1814326a21050e4d188
Greg Linton <lintong@pagodabox.com>
2015-12-10 17:09:02 -0700

    Config_files is an array

--- hookit/hooks/dev-prepare.rb
+++ hookit/hooks/dev-prepare.rb
@@ -17,7 +17,7 @@ when 'mount'
 
   # look for the 'config_files' node within the 'boxfile' payload, and
   # bind mount each of the entries
-  (boxfile[:config_files] || {}).each do |f|
+  (boxfile[:config_files] || []).each do |f|
     execute "mount #{f}" do
       command %Q(mount --bind /mnt/build/#{f} /code/#{f})
     end
@@ -28,7 +28,7 @@ when 'copy'
   logtap.print(bullet("Config 'copy' detected, running now..."), 'debug')
 
   # copy each of the values in the 'config_files' node into the raw source
-  (boxfile[:config_files] || {}).each do |f|
+  (boxfile[:config_files] || []).each do |f|
     execute "copy #{f}" do
       command %Q(cp -f /mnt/build/#{f} /code/#{f})
     end
