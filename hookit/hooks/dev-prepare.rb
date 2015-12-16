60fcd935b8c62f627229637bd8ef245ba9db8434
Tyler Flint <tylerflint@gmail.com>
2015-12-16 09:57:44 -0700

    Extract logic from dev-prepare script into NanoBox::Prepare module
    This commit doesn't change any of the existing behavior. The purpose
    of this change is to ensure the process and flow of the dev-prepare
    script is succinct and clear. By moving display message generation
    and config_file generation logic into the custom module we can
    accomplish this purpose.

--- hookit/hooks/dev-prepare.rb
+++ hookit/hooks/dev-prepare.rb
@@ -8,33 +8,33 @@ logtap.print(bullet("Running dev-prepare hook..."), 'debug')
 # By this point, engine should be set in the registry
 engine = registry('engine')
 
+# During the build process, engines can generate config
+# files that will allow the application to auto-connect
+# to services that were detected and provisioned. In dev
+# mode, those files need to be mounted or copied into
+# place.
+
 case payload[:dev_config]
 when 'none'
   logtap.print(bullet("Config 'none' detected, exiting now..."), 'debug')
-  exit 0
+
 when 'mount'
   logtap.print(bullet("Config 'mount' detected, running now..."), 'debug')
 
   # ensure boxfile[:config_files] is not empty AND at least
   # one of the files listed exists in the build
-  if boxfile[:config_files] && (config_files = existing_files(boxfile[:config_files])).any?
+  if config_files.any?
 
     # inform user
-    logtap.print  <<-END
-+> The following files were generated for your convenience and will be mounted
-   on top of your source code for the duration of this session:
-END
+    logtap.print config_mount_message
 
     # look for the 'config_files' node within the 'boxfile' payload, and
     # bind mount each of the entries
     config_files.each do |file|
-      logtap.print("     - #{file}"))
       execute "mount #{file}" do
         command %Q(mount --bind /mnt/build/#{file} /code/#{file})
       end
     end
-
-   logtap.print '   Please refer to the docs if you would like to change this behavior.'
   end
 
 when 'copy'
@@ -42,23 +42,17 @@ when 'copy'
 
   # ensure boxfile[:config_files] is not empty AND at least
   # one of the files listed exists in the build
-  if boxfile[:config_files] && (config_files = existing_files(boxfile[:config_files])).any?
+  if config_files.any?
 
     # inform user
-    logtap.print  <<-END
-+> The following files were generated for your convenience and will be copied
-   on top of your source code:
-END
+    logtap.print config_copy_message
 
     # copy each of the values in the 'config_files' node into the raw source
     config_files.each do |file|
-      logtap.print("     - #{file}"))
       execute "copy #{file}" do
         command %Q(cp -f /mnt/build/#{file} /code/#{file})
       end
     end
-
-   logtap.print '   Please refer to the docs if you would like to change this behavior.'
   end
 
 else
